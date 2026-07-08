7fd30f36d2a227b579e19d4aed6c88f7c592f46e
Coby Poluk <coby@tiereduptech.com>
2026-07-08 11:46:51 -0500

    perf: self-host fonts, remove render-blocking Google Fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/fraunces/500.css';
+import '@fontsource/fraunces/600.css';
+import '@fontsource/fraunces/700.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
